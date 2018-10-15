731bfd2bdce2eebd3cbd965bea7ca8fb9914572f
Rose <916820048@qq.com>
2018-10-15 18:58:29 +0800

    new grid

---
+++ app/src/main/java/com/example/mei/rate/GridView.java
@@ -0,0 +1,16 @@
+package com.example.mei.rate;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.shan.week3.R;
+
+public class GridView extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_view);
+    }
+
+}
